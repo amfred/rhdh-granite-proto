1dad634b8d3d94f864ebef9e4575cf8684e0cf2c
Ann Marie Fred <afred@redhat.com>
2024-12-03 08:20:07 -0500

    granite-example runs but very slowly

--- granite-example-1.py
+++ granite-example-1.py
@@ -4,6 +4,7 @@ from transformers import AutoModelForCausalLM, AutoTokenizer
 device = "auto"
 model_path = "ibm-granite/granite-3.0-8b-instruct"
 tokenizer = AutoTokenizer.from_pretrained(model_path)
+print("Your model uses the tokenizer " + type(tokenizer).__name__)
 
 # Shameless hack to get past this error:
 # RuntimeError: Expected one of cpu, cuda, ipu, xpu, mkldnn, opengl, opencl, ideep, hip, ve, fpga, maia, xla, lazy, vulkan, mps, meta, hpu, mtia, privateuseone device type at start of device string: auto
@@ -15,16 +16,23 @@ print(f"Using device: {device}")
 # drop device_map if running on CPU
 model = AutoModelForCausalLM.from_pretrained(model_path, device_map=device)
 model.eval()
+
 # change input text as desired
 chat = [
     { "role": "user", "content": "Please list one IBM Research laboratory located in the United States. You should only output its name and location." },
 ]
 chat = tokenizer.apply_chat_template(chat, tokenize=False, add_generation_prompt=True)
+# print(tokenizer.chat_template)
+
 # tokenize the text
 input_tokens = tokenizer(chat, return_tensors="pt").to(device)
+print("Generated input tokens")
+
 # generate output tokens
 output = model.generate(**input_tokens, 
                         max_new_tokens=100)
+print("Generated output tokens")
+
 # decode output tokens into text
 output = tokenizer.batch_decode(output)
 # print output
