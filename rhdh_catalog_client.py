57388b7dfaf541890ef76b61c4e827e6de5bbc68
Ann Marie Fred <afred@redhat.com>
2024-12-10 09:04:31 -0500

    added some model interaction to test client

--- rhdh_catalog_client.py
+++ rhdh_catalog_client.py
@@ -3,6 +3,11 @@ import os
 from mcp import ClientSession, StdioServerParameters
 from mcp.client.stdio import stdio_client
 
+# If this was a purely MCP test client I wouldn't need these
+import ollama
+import timeit
+import re
+
 logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger("rhdh-catalog-client")
 
@@ -12,6 +17,30 @@ server_params = StdioServerParameters(
     args=["rhdh_catalog_server.py"], # Optional command line arguments
     env=None
 )
+
+# Utility method because even when I ask the Granite model to ONLY return a URL, it usually
+# returns an explanation with the URL.
+def extract_url(string):
+    url_pattern = r"https?:\/\/(www\.)?[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_\+.~#?&//=]*)"
+    match = re.search(url_pattern, string)
+    if match:
+        return match.group(0)
+    else:
+        return None
+
+# Utility method to call the local granite model
+async def call_granite_on_ollama(prompt) -> str:
+    start = timeit.default_timer()
+    response = ollama.chat(model='granite3-dense:8b', messages=[
+    {
+        'role': 'user',
+        'content': prompt,
+    }])
+    inferenceTime = timeit.default_timer() - start
+    responseStr = response['message']['content']
+    logger.info(f"The inference service response is: \n{responseStr}")
+    logger.info(f"The inference execution time is: {inferenceTime}")
+    return responseStr
 
 async def run():
     async with stdio_client(server_params) as (read, write):
@@ -72,6 +101,25 @@ async def run():
                 "apiKey": os.environ.get('RHDH_API_KEY', None),
                 })
             logger.info(f"Inference server list from RHDH API: \n{result.content[0].text}")
+
+            # This section isn't using MCP yet; it's just demonstrating that a model
+            # can help to parse the data that comes back from Developer Hub.
+            # Ask the Granite model to do something with the catalog results
+
+            signupResponse = await call_granite_on_ollama('Given the following context, where should I go to sign up for the 3scale-based developer-model-service? Respond with ONLY the URL:\n'+result.content[0].text)
+            logger.info(f"The service sign-up response is: \n{signupResponse}")
+
+            ollamaResponse = await call_granite_on_ollama('Given the following context, what is the ollama inference server URL? Respond with ONLY the URL:\n'+result.content[0].text)
+            logger.info(f"The inference service response is: \n{ollamaResponse}")
+
+            vllmResponse = await call_granite_on_ollama('Given the following context, what is the VLLM inference server URL? Respond with ONLY the URL:\n'+result.content[0].text)
+            logger.info(f"The inference service response is: \n{vllmResponse}")
+
+            logger.info(f"The service sign-up URL is: {extract_url(signupResponse)}")
+            logger.info(f"The ollama inference service URL is: {extract_url(ollamaResponse)}")
+            logger.info(f"The VLLM inference service URL is: {extract_url(vllmResponse)}")
+            
+
 
 if __name__ == "__main__":
     import asyncio
